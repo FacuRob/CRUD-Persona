d147fb20257981d06fa2e4951faa16973e3892b6
FacuRob <134459445+FacuRob@users.noreply.github.com>
2024-07-13 19:33:29 -0300

    Update db.js

--- data/db.js
+++ data/db.js
@@ -2,10 +2,16 @@ const {Sequelize} = require("sequelize")
 //const db = new Sequelize("codoacodo", "root","", {
 
 const db = new Sequelize("it-soluciones_codoacodo", "368105","3]GeA4Yz_g?d8@X", {
-//    host: "localhost",
     host: "mysql-it-soluciones.alwaysdata.net",
     dialect: "mysql",
     port: 3306
 })
+
+//Modo Local
+/*const db = new Sequelize("codoacodo", "root","", {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306
+})*/
 
 module.exports = db
